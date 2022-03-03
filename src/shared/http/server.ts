5d477fbf37b0a578607f17c4b8ae5476056802a5
Benhur Zambiasi <benhur_zambiasi@hotmail.com>
2022-03-03 01:43:37 -0300

    first commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ message: error.message, status: 'error' });
+  }
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal Server Error' });
+});
+
+app.listen(3333, () => {
+  console.log('Server listening on port 3333!');
+});
