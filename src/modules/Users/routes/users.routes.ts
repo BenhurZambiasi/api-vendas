533dbd5a4b2d0cedddb00eb36bf50bf118c52799
Benhur <benhur_zambiasi@hotmail.com>
2022-03-03 16:33:56 -0300

    gerando hash para senha

--- src/modules/Users/routes/users.routes.ts
+++ src/modules/Users/routes/users.routes.ts
@@ -21,9 +21,8 @@ usersRouter.post(
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().required(),
-      email: Joi.string().required(),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
-      avatar: Joi.string(),
     },
   }),
   usersController.create,
