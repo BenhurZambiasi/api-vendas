bb37f9d9918a71171deec4684adf5e500ea3f25a
Benhur Zambiasi <benhur_zambiasi@hotmail.com>
2022-03-05 03:40:00 -0300

    email

---
+++ src/modules/Users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,28 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import EtherealMail from '@config/mail/EtherealMail';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokenRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+    const token = await userTokenRepository.generate(user.id);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
+  }
+}
+export default SendForgotPasswordEmailService;
